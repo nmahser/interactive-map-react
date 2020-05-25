d7d3e04df5682d18b74c3f64456e897536d9120c
nmahser <nmahser@mail.usf.edu>
2020-05-24 23:59:00 -0400

    created User component

---
+++ src/js/components/User/User.js
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
+
+export function User() {
+  // hooks for random user data
+  const [user, setUser] = useState(null);
+  // hooks for button
+  const [showData, setShowData] = useState(false);
+
+  // make Api call for random userId and title
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((res) => res.json())
+      .then((result) => {
+        setUser(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  // get current date
+  const date = new Date();
+  const dateString =
+    date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+
+  return (
+    <div className="user">
+      {user && (
+        <div className="user-detail">
+          <p id="user-id">USER ID: {user.id}</p>
+          <p>TITLE: {user.title}</p>
+        </div>
+      )}
+      <div className="user-button">
+        <button onClick={() => setShowData(true)}> click me</button>
+        {showData && <p>MAP CREATED BY: Nihat Mahser on {dateString}</p>}
+      </div>
+    </div>
+  );
+}
