c401e774e03fe45c29ab218bb9df15abae57c7df
nmahser <nmahser@mail.usf.edu>
2020-05-24 23:30:23 -0400

    added Sources and Layers

--- src/js/components/Map/Map.js
+++ src/js/components/Map/Map.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { useState } from "react";
-import MapGL from "react-map-gl";
+import MapGL, { Source, Layer } from "react-map-gl";
+import { usIncome } from "../../../data/incomeData";
+import { stateCapitals } from "../../../data/stateCapitals";
 import { BASIC_STYLE } from "./Style";
 import { apiKeyMapBox } from "../../../config";
 
@@ -16,11 +18,47 @@ export function Map() {
 
   const MAPBOX_TOKEN = apiKeyMapBox;
 
+  // inline style for div tag below
+  const divStyle = {
+    height: "100%",
+    position: "relative",
+  };
+
+  // usIncome Layer
+  const fill = {
+    id: "usIncome",
+    type: "fill",
+    source: "usIncome",
+    paint: {
+      "fill-opacity": 0.2,
+    },
+  };
+
+  // stateCapitals Layer
+  const circle = {
+    id: "stateCapitals",
+    type: "circle",
+    source: "stateCapitals",
+    paint: {
+      "circle-color": "#B22222",
+    },
+  };
+
   return (
-    <MapGL
-      {...viewport}
-      mapboxApiAccessToken={MAPBOX_TOKEN}
-      onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    ></MapGL>
+    <div className="app" style={divStyle}>
+      <MapGL
+        {...viewport}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      >
+        <Source id="usIncome" type="geojson" data={usIncome}>
+          <Layer {...fill} />
+        </Source>
+
+        <Source id="stateCapitals" type="geojson" data={stateCapitals}>
+          <Layer {...circle} />
+        </Source>
+      </MapGL>
+    </div>
   );
 }
