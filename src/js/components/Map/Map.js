b632c65efafd0498e3c988ac05b60e966afc8b9f
nmahser <nmahser@mail.usf.edu>
2020-05-25 00:24:54 -0400

    added mapStyle

--- src/js/components/Map/Map.js
+++ src/js/components/Map/Map.js
@@ -78,6 +78,7 @@ export function Map() {
       <div className="map">
         <MapGL
           style={mapGl}
+          mapStyle={mapStyle}
           {...viewport}
           mapboxApiAccessToken={MAPBOX_TOKEN}
           onViewportChange={(nextViewport) => setViewport(nextViewport)}
