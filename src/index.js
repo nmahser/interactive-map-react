d2ec80b64b5e97c9cb9c3a903c816b3ec5cc121f
nmahser <nmahser@mail.usf.edu>
2020-05-25 00:17:14 -0400

    imported index.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Map } from "./js/components/Map/Map";
+import "./index.css";
 
 ReactDOM.render(<Map />, document.getElementById("container"));
